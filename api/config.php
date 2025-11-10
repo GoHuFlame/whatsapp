b639074579f5bfd843c01ce1f5b9dae6a88d515d
Morakz <63670601+GoHuFlame@users.noreply.github.com>
2025-11-10 13:07:32 -0600

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_url' => getenv('MERCATELY_API_URL')',
-    'api_key' => getenv('MERCATELY_API_KEY')', 
-    'internal_id' => getenv('MERCATELY_INTERNAL_ID')', 
+    'api_url' => getenv('MERCATELY_API_URL'),
+    'api_key' => getenv('MERCATELY_API_KEY'),
+    'internal_id' => getenv('MERCATELY_INTERNAL_ID'),
 ];
